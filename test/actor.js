9e6a378519e7f663fb53253660f91120d93e76e2
Jean Hugues Robert <jean_hugues_robert@yahoo.com>
2013-01-09 14:51:20 -0500

    actor.js, local mode

--- test/actor.js
+++ test/actor.js
@@ -17,12 +17,16 @@ var mand = l8.mand
 
 l8.trace( "Actor starting")
 
+/* ---------------------------------------------------------------------------
+ *  Actors, local
+ */
+
 var AllActors = {}
 
-function Actor( name, pattern ){
+function Actor( name ){
   this.name    = name
   this.task    = null
-  this.queue   = l8.queue()
+  this.queue   = l8.queue() // aka "mailbox"
   this.backlog = []
   this.pattern = null
   this.picking = false
@@ -31,7 +35,6 @@ function Actor( name, pattern ){
     previous.task.cancel()
   }
   AllActors[name] = this
-  this.receive( pattern)
   return this
 }
 var ProtoActor = Actor.prototype
@@ -52,7 +55,7 @@ ProtoActor.pick = function(){
     if( !(msg = list[ii]) )continue
     empty = false
     try{
-      this.pattern.apply( this.task, msg)
+      this.pattern.apply( this, msg)
     }catch( TypeError ){ continue }
     found = true
     list[ii] = null
@@ -67,10 +70,7 @@ ProtoActor.pick = function(){
 
 ProtoActor.act = function(){
   var that = this
-  l8.defer( function(){
-    AllActors[that.name] = null
-  })
-  .repeat( function(){
+  l8.repeat( function(){
     // First, check backlog
     this.step( function(){
       if( that.pick() ) this.continue
@@ -80,7 +80,7 @@ ProtoActor.act = function(){
     // Either a match or add to backlog
     }).step( function( msg ){
       try{
-        that.pattern.apply( that.task, msg)
+        that.pattern.apply( that, msg)
       }catch( TypeError ){
         l8.de&&bug.apply( this, ["backlog"].concat( msg))
         that.backlog.push( msg)
@@ -94,15 +94,22 @@ ProtoActor.act = function(){
 
 ProtoActor.receive = function( pattern ){
 // Define new behavior of actor using patterns
-  // Encapsulate patterns to handle exception my way
-  for( var attr in pattern ){
-    pattern[attr] = (function( block){
-      return function(){
-        try{ block.apply( this, arguments) }catch( e ){ l8.raise( e, true) }
-      }
-    })( pattern[attr])
-  }
-  this.pattern = Pattern( pattern)
+  // Restore current pattern when task terminates
+  var previous_pattern = this.pattern
+  var that = this
+  this.task._task( function(){
+    this.defer( function(){ that.pattern = previous_pattern})
+    // Encapsulate patterns to handle exception my way
+    for( var attr in pattern ){
+      pattern[attr] = (function( block){
+        return function(){
+          try{ block.apply( this, arguments) }catch( e ){ l8.raise( e, true) }
+        }
+      })( pattern[attr])
+    }
+    that.pattern = Pattern( pattern)
+    that.act()
+  })
 }
 
 ProtoActor.send = function(){
@@ -112,9 +119,12 @@ ProtoActor.send = function(){
 
 function MakeActorConstructor( name, pattern ){
   return function(){
-    var act = new Actor( name, pattern)
-    var task = l8.spawn( function(){
-      act.act()
+    de&&bug( "create actor " + name)
+    var act = new Actor( name)
+    var task = l8._spawn( function(){
+      task.var( "actor", act)
+      task.step( function(){ act.receive( pattern) })
+      task.step( function(){ task.join() })
     })
     return act.task = task
   }
@@ -124,16 +134,17 @@ function MakeActorConstructor( name, pattern ){
  *  Exports are added to existing l8 object
  */
  
-l8.Actor        = MakeActorConstructor
-l8.Actor.lookup = Actor.lookup
+l8.Actor         = MakeActorConstructor
+l8.Actor.lookup  = Actor.lookup
+l8.Actor.all     = AllActors
+l8.proto.__defineGetter__( "actor", function(){ return this.get( "actor")})
 
 /*
  *  Example. A "logging" actor
  */
  
 var Logger = l8.Actor( "l8_logger", {
-  '"error", x': function( x ){ 
-      return l8.trace( "Error: " + x) },
+  '"error", x': function( x ){ return l8.trace( "Error: " + x) },
   'x':          function( x ){ return l8.trace( x) },
   '"throw", e': function( e ){ l8.trace( "throw...", e); throw e }
   //'...': function(){ l8.trace( "unsupported) }
@@ -152,7 +163,17 @@ mylog.send( "Hello")
 mylog.send( "error", "is ok")
 mylog.send( "does not understand", "this")
 mylog.send( "throw", "something that kills")
-l8.countdown( 1)
+l8.countdown( 2)
+
+/* ---------------------------------------------------------------------------
+ *  Stages, with actors in them. Each nodejs process (or browser) is a stage.
+ */
+ 
+/* ---------------------------------------------------------------------------
+ *  Campaign, where actors on multiple stages cooperate
+ */
+
+
 
 
 
