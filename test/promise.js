52e9dbc73ef097b8568ae633f03085d107b757ce
Jean Hugues Robert <jean_hugues_robert@yahoo.com>
2013-05-03 13:03:36 -0400

    fix missing test/promise.js in build

---
+++ test/promise.js
@@ -0,0 +1,14 @@
+// promise.js
+//   adapter for promises-aplus/promises-tests
+// See https://github.com/promises-aplus/promises-tests
+
+var Parole = require( "l8/lib/whisper.js" ).parole;
+
+module.exports.pending = function(){
+  var p = Parole();
+  return {
+    promise: p,
+    fulfill: function( v ){ return p.resolve( v ); },
+    reject:  function( r ){ return p.reject(  r ); }
+  };
+};
