6bcf318a4580799bf679c95077ffc8f72d844d4d
Jean Hugues Robert <jean_hugues_robert@yahoo.com>
2014-01-23 08:48:58 -0500

    Small fixes to pass all 872 tests of latest promises-test package

--- test/promise.js
+++ test/promise.js
@@ -4,11 +4,20 @@
 
 var Parole = require( "l8/lib/whisper" );
 
-module.exports.pending = function(){
+module.exports.deferred = function(){
   var p = Parole();
   return {
     promise: p,
-    fulfill: function( v ){ return p.resolve( v ); },
+    resolve: function( v ){ return p.resolve( v ); },
     reject:  function( r ){ return p.reject(  r ); }
   };
 };
+var promisesAplusTests = require("promises-aplus-tests");
+
+// If debugging
+if( 0 ){
+  require( "mocha-unfunk-reporter" );
+  promisesAplusTests(module.exports, { reporter: "mocha-unfunk-reporter", grep: "" }, function (err) {
+    // All done; output is in the console. Or check `err` for number of failures.
+  });
+}
